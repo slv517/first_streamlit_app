f8a1117c14b812399418c26bb5984ddd2db31e06
slv517 <86605813+slv517@users.noreply.github.com>
2023-04-14 17:03:50 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -49,3 +49,23 @@ my_cur.execute("SELECT * FROM fruit_load_list")
 my_data_rows = my_cur.fetchall()
 streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_rows)
+
+
+
+
+# New section to display fruityvice api response
+streamlit.header('Fruityvice Fruit Advice!')
+fruit_choice = streamlit.text_input("What fruit would you like information about?", "jackfruit")
+streamlit.write("The user entered", fruit_choice)
+
+
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+
+
+# take the json version of the response and normalise it
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+
+streamlit.text("Thanks for adding jackfruit")
+
+
